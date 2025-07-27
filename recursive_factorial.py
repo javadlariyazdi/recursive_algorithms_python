95b67da093c00564d91d23749c10f8281b820a78
javadlariyazdi <javad.lariyazdi.dev@gmail.com>
2025-07-27 22:44:50 +0330

    recursive_factorial

--- recursive_factorial.py
+++ recursive_factorial.py
@@ -3,9 +3,9 @@
 #recursive_factorial
 
 def factorial(n):
-    if n == 0:
-        return 0
-    return factorial(n-1) + n
+    if n == 0 or n == 1:
+        return 1
+    return factorial(n-1) * n
 
 n = int(input("enter a number: "))
 print(factorial(n))
