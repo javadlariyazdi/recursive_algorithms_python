213ce112cc88dcc01aaad60af8bf2320b3f4e523
javadlariyazdi <javad.lariyazdi.dev@gmail.com>
2025-08-06 20:47:06 +0330

    feat: handle input validation and RecursionError in factorial function

--- recursive_factorial.py
+++ recursive_factorial.py
@@ -7,5 +7,16 @@ def factorial(n):
         return 1
     return factorial(n-1) * n
 
-n = int(input("enter a number: "))
-print(factorial(n))
+try:
+    n = int(input("enter a number: "))
+    if n < 0:
+         raise ValueError("⚠️ please enter positive number! ⚠️")
+    result = factorial(n)
+except ValueError as ve:
+    print(f"❌ Input Error:", {ve}, "❌")  
+except RecursionError:
+    print("❌ Input is too large! Maximum recursion depth exceeded. ❌")
+except Exception as e:
+    print(f"hmmm ⚠️ Unexpected Error: {e} ⚠️")    
+else:
+    print(f"✅ Result:{result}")
