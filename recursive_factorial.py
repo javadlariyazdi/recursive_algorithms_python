39ce060edfa03c2e69f6c6ea144e24cdc0849f46
javadlariyazdi <javad.lariyazdi.dev@gmail.com>
2025-08-11 21:56:32 +0330

    recursive_factorial

--- recursive_factorial.py
+++ recursive_factorial.py
@@ -7,16 +7,17 @@ def factorial(n):
         return 1
     return factorial(n-1) * n
 
-try:
-    n = int(input("enter a number: "))
-    if n < 0:
-         raise ValueError("⚠️ please enter positive number! ⚠️")
-    result = factorial(n)
-except ValueError as ve:
-    print(f"❌ Input Error:", {ve}, "❌")  
-except RecursionError:
-    print("❌ Input is too large! Maximum recursion depth exceeded. ❌")
-except Exception as e:
-    print(f"hmmm ⚠️ Unexpected Error: {e} ⚠️")    
-else:
-    print(f"✅ Result:{result}")
+if __name__ == "__main__":
+    try:
+        n = int(input("enter a number: "))
+        if n < 0:
+            raise ValueError("⚠️ please enter a non-negative number! ⚠️")
+        result = factorial(n)
+    except ValueError as ve:
+        print(f"❌ Input Error:", {ve}, "❌")  
+    except RecursionError:
+        print("❌ Input is too large! Maximum recursion depth exceeded. ❌")
+    except Exception as e:
+        print(f"hmmm ⚠️ Unexpected Error: {e} ⚠️")    
+    else:
+        print(f"✅ Result:{result}")
