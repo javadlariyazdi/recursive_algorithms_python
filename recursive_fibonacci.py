ef42e3de2990585ef8e573973b0326609ea45a4b
javadlariyazdi <javad.lariyazdi.dev@gmail.com>
2025-08-12 23:35:05 +0330

    recursive_fibonacci

--- recursive_fibonacci.py
+++ recursive_fibonacci.py
@@ -2,21 +2,22 @@
 
 def fibonacci(n):
     if n == 0:
-        return 0
+        return 1
     if n == 1:
         return 1
     return fibonacci(n-2) + fibonacci(n-1)
 
-try:
-    n = int(input("enter a number: "))
-    if n < 0:
-         raise ValueError("⚠️ please enter positive number! ⚠️")
-except ValueError as ve:
-    print(f"❌ Input Error:", {ve}, "❌")  
-except RecursionError:
-    print("❌ Input is too large! Maximum recursion depth exceeded. ❌")
-except Exception as e:
-    print(f"hmmm ⚠️ Unexpected Error: {e} ⚠️")    
-else:
-    for i in range(n+1):
-        print(fibonacci(i))
+if __name__ == "__main__":
+    try:
+        n = int(input("enter a number: "))
+        if n < 0:
+            raise ValueError("⚠️ please enter positive number! ⚠️")
+    except ValueError as ve:
+        print(f"❌ Input Error: {ve} ❌")  
+    except RecursionError:
+        print("❌ Input is too large! Maximum recursion depth exceeded. ❌")
+    except Exception as e:
+        print(f"hmmm ⚠️ Unexpected Error: {e} ⚠️")    
+    else:
+        for i in range(n):
+            print(fibonacci(i))
