27e943752ff301212e4a30dcc59f780ca6047341
javadlariyazdi <javad.lariyazdi.dev@gmail.com>
2025-08-15 22:39:12 +0330

    recursive_sum_to_n

--- recursive_sum_to_n.py
+++ recursive_sum_to_n.py
@@ -2,10 +2,22 @@
 #recursive_sum_to_n
 
 def recursive_sum_to_n(n):
+    """Return the sum of all numbers from 1 to n using recursion."""
     if n == 0:
         return 0
     return recursive_sum_to_n(n-1) + n
 
-n = int(input("enter a number: "))
-print(recursive_sum_to_n(n))
+if __name__ == "__main__":
+    try:
+        n = int(input("enter a number:"))
+        if n < 0 :
+            raise ValueError("⚠️ please enter positive number! ⚠️")
+    except ValueError as ve:
+        print(f"❌ Input Error: {ve} ❌")  
+    except RecursionError:
+        print("❌ Input is too large! Maximum recursion depth exceeded. ❌")
+    except Exception as e:
+        print(f"⚠️ Unexpected Error: {e} ⚠️")       
+    else:
+        print(f"✅sum numbers: {recursive_sum_to_n(n)}")
         
