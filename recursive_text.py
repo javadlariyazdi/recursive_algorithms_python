9850116494acdebaf4e593c3e454535faab015fb
javadlariyazdi <javad.lariyazdi.dev@gmail.com>
2025-08-15 22:42:23 +0330

    recursive_text

--- recursive_text.py
+++ recursive_text.py
@@ -1,10 +1,11 @@
 
 #recursive_text
 def recursive_text(word):
+    """Return the reverse of a string using recursion."""
     if word == "":
         return ""
     return word[-1] + recursive_text(word[:-1])
         
-
-word = input('please enter your text: ')
-print(recursive_text(word))
+if __name__ == "__main__":
+    word = input('please enter your text: ')
+    print(recursive_text(word))
